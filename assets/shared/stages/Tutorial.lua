2df25e232d34c6ed36adc09511ec46e2d9273bae
Binpuki <jpaulvu@gmail.com>
2024-02-13 16:07:36 -0600

    manual

---
+++ assets/shared/stages/Tutorial.lua
@@ -0,0 +1,29 @@
+function onCreate()
+
+    makeLuaSprite("sky", 'Bg', -500, -250)
+    addLuaSprite("sky", false)
+
+    makeLuaSprite("Bg mountains", 'Bg mountains', -500, -250)
+    addLuaSprite("Bg mountains", false)
+
+    makeLuaSprite("mountains", 'mountains', -500, -250)
+    addLuaSprite("mountains", false)
+
+    makeLuaSprite("floor", 'Floor', -500, -250)
+    addLuaSprite("floor", false)
+
+    makeLuaSprite("floor glow", 'Floor glow', -500, -250)
+    addLuaSprite("floor glow", false)
+
+    setProperty("Bg mountains.alpha", 0.5)
+    setProperty("mountains.alpha", 0.5)
+    setScrollFactor("Bg mountains", 0.7, 1)
+    setScrollFactor("mountains", 0.9, 1)
+
+end
+
+function onCreatePost()
+    setProperty('boyfriend.visible', false)
+end
+
+    
