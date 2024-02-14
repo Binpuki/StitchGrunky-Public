b31b4afb35b8a02697af35ffa27f2b5cd08dffcf
Goompld <emaraspsuar@outlook.com>
2024-02-13 23:33:41 -0800

    f

--- assets/shared/characters/stitch/bf-stitch.lua
+++ assets/shared/characters/stitch/bf-stitch.lua
@@ -1,6 +1,6 @@
 function onCreate()
 	setPropertyFromClass('substates.GameOverSubstate', 'characterName', 'bf-stitch'); --Character json file for the death animation
-	setPropertyFromClass('substates.GameOverSubstate', 'deathSoundName', 'stitch/amongkill'); --put in mods/sounds/
+	setPropertyFromClass('substates.GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx'); --put in mods/sounds/
 	setPropertyFromClass('substates.GameOverSubstate', 'loopSoundName', 'stitch/silence'); --put in mods/music/
 	setPropertyFromClass('substates.GameOverSubstate', 'endSoundName', 'stitch/bwopm'); --put in mods/music/
 end
