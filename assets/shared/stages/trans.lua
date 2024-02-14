da3bcd8dccb3647ca69f0e5744fdb454a2b6c9af
Binpuki <jpaulvu@gmail.com>
2024-02-13 20:58:58 -0600

    whoops

--- assets/shared/stages/trans.lua
+++ assets/shared/stages/trans.lua
@@ -1,28 +1,28 @@
 function onCreate()
-    makeLuaSprite("sky", 'stages/trans/white', -500, -100)
+    makeLuaSprite("sky", 'white', -500, -100)
     addLuaSprite("sky", false)
 
-    makeLuaSprite("pinkg", 'stages/trans/pink', -500, -100)
+    makeLuaSprite("pinkg", 'pink', -500, -100)
     addLuaSprite("pinkg", false)
 
-    makeLuaSprite("putple", 'stages/trans/purplegradient', -500, -100)
+    makeLuaSprite("putple", 'purplegradient', -500, -100)
     addLuaSprite("putple", false)
 
-    makeLuaSprite("bg", 'stages/trans/mountainbg', -500, -100)
+    makeLuaSprite("bg", 'mountainbg', -500, -100)
     addLuaSprite("bg", false)
 
-    makeLuaSprite("mountain", 'stages/trans/bgmountain', -500, -100)
+    makeLuaSprite("mountain", 'bgmountain', -500, -100)
     addLuaSprite("mountain", false)
 
-    makeLuaSprite("bgROCK", 'stages/trans/rock3', -500, -100)
+    makeLuaSprite("bgROCK", 'rock3', -500, -100)
     addLuaSprite("bgROCK", false)
 
-    makeLuaSprite("bgROCK2", 'stages/trans/rock3straight', -500, -100)
+    makeLuaSprite("bgROCK2", 'rock3straight', -500, -100)
     addLuaSprite("bgROCK2", false)
 
-    makeLuaSprite("bgROCK3", 'stages/trans/rock2', -500, -100)
+    makeLuaSprite("bgROCK3", 'rock2', -500, -100)
     addLuaSprite("bgROCK3", false)
 
-    makeLuaSprite("bgROCK14", 'stages/trans/rock1', -500, -100)
+    makeLuaSprite("bgROCK14", 'rock1', -500, -100)
     addLuaSprite("bgROCK14", false)
 end
