c3457af20f8733a4c42da15428cbb86273329321
Goompld <emaraspsuar@outlook.com>
2024-02-13 22:09:13 -0800

    TRANNSNSS

--- assets/shared/stages/trans.lua
+++ assets/shared/stages/trans.lua
@@ -8,27 +8,36 @@ function onCreate()
     makeLuaSprite("putple", 'purplegradient', -500, -100)
     addLuaSprite("putple", false)
 
-    makeLuaSprite("bg", 'mountainbg', -500, -100)
+    makeLuaSprite("bg", 'mountainbg', -400, -100)
     addLuaSprite("bg", false)
 
-    makeLuaSprite("mountain", 'bgmountain', -500, -100)
+    makeLuaSprite("mountain", 'bgmountain', -350, -100)
     addLuaSprite("mountain", false)
 
-    makeLuaSprite("bgROCK", 'rock3', -500, -100)
+    makeLuaSprite("bgROCK", 'rock3', -650, -300)
     addLuaSprite("bgROCK", false)
 
-    makeLuaSprite("bgROCK2", 'rock3straight', -500, -100)
+    makeLuaSprite("bgROCK2", 'rock3straight', -650, -300)
     addLuaSprite("bgROCK2", false)
 
-    makeLuaSprite("bgROCK3", 'rock2', -500, -100)
+    makeLuaSprite("bgROCK3", 'rock2', -650, -300)
     addLuaSprite("bgROCK3", false)
 
-    makeLuaSprite("bgROCK14", 'rock1', -500, -100)
+    makeLuaSprite("bgROCK14", 'rock1', -650, -300)
     addLuaSprite("bgROCK14", false)
 
-    makeLuaSprite("floor", "floor", -235, 570)--im gonna kill myself, someone please check the code please please please please
+    setScrollFactor("bgROCK", 0.7, 1.05)
+    setScrollFactor("bgROCK2", 0.725, 1.05)
+    setScrollFactor("bgROCK3", 0.75, 1.05)
+    setScrollFactor("bgROCK14", 0.775, 1.05)
+    setScrollFactor("mountain", 0.5, 1)
+    setScrollFactor("bg", 0.3, 1)
+    setScrollFactor("sky", 0.4, 1.1)
+    setScrollFactor("pinkg", 0.4, 1.15)
+    setScrollFactor("putple", 0.4, 1.2)
+
+    makeLuaSprite("floor", "floor", -600, -325)
     addLuaSprite("floor", false)
-	scaleObject("floor", 0.625, 0.6, true)
 
     makeLuaSprite('UpperBar', '', 0, -130) --Default value of the y is -130
 	makeGraphic('UpperBar', 1500, 550, '000000')
@@ -45,4 +54,12 @@ function onCreate()
 	setProperty('LowerBar.color', "0x000000")
 	addLuaSprite('LowerBar', true);
 	setProperty("LowerBar.y", -450);
+end
+
+function onUpdate(elapsed)
+    if mustHitSection then 
+        setProperty("defaultCamZoom", 1.1)
+    else
+        setProperty("defaultCamZoom", 1.35)
+    end
 end
