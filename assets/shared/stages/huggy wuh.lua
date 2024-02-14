d77f7923522a32504b3054746fbec27e03c5a1a1
Goompld <emaraspsuar@outlook.com>
2024-02-13 23:15:46 -0800

    FINAL FUCKING HUGGY FUCKY PUSH FUCK.

--- assets/shared/stages/huggy wuh.lua
+++ assets/shared/stages/huggy wuh.lua
@@ -2,4 +2,6 @@ function onCreate()
     makeLuaSprite("bACK", 'huggybgg', -1000, -550)
     addLuaSprite("bACK", false)
     scaleLuaSprite("bACK", 1.3, 1.3)
+    setProperty('iconP1.alpha', tonumber(0))
+    setProperty('iconP2.alpha', tonumber(0))
 end
