d85bc448995fff5917bd3595472ed097ca941479
Goompld <emaraspsuar@outlook.com>
2024-02-13 23:12:11 -0800

    PLEASE FUCKING BE THE LAST HUGGY FUCKY PUSH

--- assets/shared/stages/huggy wuh.lua
+++ assets/shared/stages/huggy wuh.lua
@@ -1,5 +1,5 @@
 function onCreate()
-    makeLuaSprite("bACK", 'stages/wuggy/huggybgg', -1000, -550)
+    makeLuaSprite("bACK", 'huggybgg', -1000, -550)
     addLuaSprite("bACK", false)
     scaleLuaSprite("bACK", 1.3, 1.3)
 end
