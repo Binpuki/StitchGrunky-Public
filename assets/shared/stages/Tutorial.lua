ea2fa9c2fbfcfbc3ba435911db0868982eb3152b
Binpuki <jpaulvu@gmail.com>
2024-02-13 19:07:46 -0600

    MAMNUAL

--- assets/shared/stages/Tutorial.lua
+++ assets/shared/stages/Tutorial.lua
@@ -3,7 +3,7 @@ function onCreate()
     makeLuaSprite("sky", 'Bg', -500, -250)
     addLuaSprite("sky", false)
 
-    makeLuaSprite("Bg mountains", 'Bg mountains', -500, -250)
+    makeLuaSprite("Bg mountains", 'bgmountains', -500, -250)
     addLuaSprite("Bg mountains", false)
 
     makeLuaSprite("mountains", 'mountains', -500, -250)
@@ -12,7 +12,7 @@ function onCreate()
     makeLuaSprite("floor", 'Floor', -500, -250)
     addLuaSprite("floor", false)
 
-    makeLuaSprite("floor glow", 'Floor glow', -500, -250)
+    makeLuaSprite("floor glow", 'floorglow', -500, -250)
     addLuaSprite("floor glow", false)
 
     setProperty("Bg mountains.alpha", 0.5)
