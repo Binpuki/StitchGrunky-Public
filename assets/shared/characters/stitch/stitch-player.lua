49bb64e7f6d7279a526f860e3040ace5e91fb741
Goompld <emaraspsuar@outlook.com>
2024-02-13 23:24:42 -0800

    huggy fucky and stitch die soungs

--- assets/shared/characters/stitch/stitch-player.lua
+++ assets/shared/characters/stitch/stitch-player.lua
@@ -1,6 +1,6 @@
 function onCreate()
 	setPropertyFromClass('substates.GameOverSubstate', 'characterName', 'stitch-player'); --Character json file for the death animation
-	setPropertyFromClass('substates.GameOverSubstate', 'deathSoundName', 'stitch/amongkill'); --put in mods/sounds/
+	setPropertyFromClass('substates.GameOverSubstate', 'deathSoundName', 'stitch/stitch_death'); --put in mods/sounds/
 	setPropertyFromClass('substates.GameOverSubstate', 'loopSoundName', 'stitch/silence'); --put in mods/music/
-	setPropertyFromClass('substates.GameOverSubstate', 'endSoundName', 'stitch/bwopm'); --put in mods/music/
+	setPropertyFromClass('substates.GameOverSubstate', 'endSoundName', 'stitch/stitch_confirm'); --put in mods/music/
 end
